e4309a7d4420a972e703bbf632f4c516f3b7e936
Alexey Govorov <algovor04@gmail.com>
2026-09-09 17:00:42 +0700

    Попытка получить из контейнера зависимость с транзитивными зависимостями

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use App\Database\Database;
 use App\Container\Container;
+use App\Controller\UserController;
 
 $container = new Container();
-$database = $container->get(Database::class);
+$database = $container->get(UserController::class);
 
 var_dump($database);
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,13 @@
 
 namespace App\Controller;
 
+use App\Service\UserService;
+
 class UserController {
-    
+    private UserService $service;
+
+    public function __construct(UserService $service)
+    {
+        $this->service = $service;
+    }
 }
