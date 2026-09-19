cef6e5f1305c06302dac79c2a23ec2cec486d31c
Alexey Govorov <algovor04@gmail.com>
2026-09-19 15:31:48 +0700

    Убрана ненужная зависимость Database в UserRepository

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -2,18 +2,10 @@
 
 namespace App\Repository;
 
-use App\ORM\Database;
 use App\Models\User;
 use Override;
 
 class UserRepository implements UserRepositoryInterface {
-    private Database $database;
-
-    public function __construct(Database $database)
-    {
-        $this->database= $database;
-    }
-
     #[Override]
     public function getAll(): array
     {
